29f2695644b2f1d6f43826fe5859ddd73ce325ac
n2m2ng <n2m2ng@example.com>
2019-02-18 19:15:54 +0900

    second commit

---
+++ file_write.py
@@ -0,0 +1,3 @@
+write_file = open('output.txt', 'w')
+write_file.write('Hello World!\n')
+write_file.close()
---
+++ pybot_file.py
@@ -0,0 +1,4 @@
+text_file = open('pybot.txt', encoding='utf-8')
+raw_data = text_file.read()
+text_file.close()
+print(raw_data)
